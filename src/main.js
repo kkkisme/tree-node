51d9c4232f67580f8ea8f9720ba33eb3d13b63a7
huming <ming1.hu@meicloud.com>
2018-05-20 12:10:52 +0800

    chore: change eslint config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import Vue from "vue"
-import router from "./routes"
+import Vue from 'vue'
+import router from './routes'
 import App from './App'
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app")
+}).$mount('#app')
